e31d33fca5e2c5c83eb9b4d4c8b47816db0d88e7
Adarsh Kumar <adarshkr1291@gmail.com>
2026-03-01 22:30:24 +0530

    Add SQL queries for  business Problems

---
+++ Customer_Retention_problem.sql
@@ -0,0 +1,51 @@
+
+-- =====================================================
+-- CUSTOMER RETENTION ANALYSIS PROJECT
+-- =====================================================
+
+
+-- ================================
+-- BUSINESS OVERVIEW
+-- ================================
+
+--  Calculate the total number of distinct customers.
+--  Calculate the overall customer retention rate (in percentage).
+--  Compute the retention rate for each city.
+--  Show the distribution of customers by favorite order day.
+--  Identify the top 5 cities with the highest customer count.
+
+
+-- ================================
+--  RETAINED VS NON-RETAINED ANALYSIS
+-- ================================
+
+--  Compare the average order value between retained and non-retained customers.
+--  Compare the average order frequency between retained and non-retained customers.
+--  Analyze the difference in email open rate between retained and non-retained customers.
+--  Evaluate the impact of email click rate on customer retention.
+--  Calculate the retention rate among customers using the paperless service.
+
+
+-- ================================
+-- BEHAVIORAL ANALYSIS
+-- ================================
+
+--  Calculate the retention rate among customers with above-average order frequency.
+--  Calculate the retention rate among customers with above-average order value.
+--  Determine the retention status of customers whose last order was more than 6 months ago.
+--  Calculate the average active duration (in days) between first order and last order.
+--  Analyze the month-wise customer signup trend.
+
+
+-- ================================
+-- ADVANCED & ANALYTICAL INSIGHTS
+-- ================================
+
+--  Identify the top 10 retained customers based on estimated revenue (avgorder × ordfreq).
+--  Compare total revenue contribution between retained and non-retained customers.
+--  Identify high-value customers who are not retained (potential churn risk).
+--  Rank cities based on retention rate using a window function.
+--  Segment customers into High, Medium, and Low value categories based on order value and frequency.
+
+
+
